c161863d75f7a3e57e5990567838f22cef2b6001
annay <akyoung@byu.edu>
2025-03-19 15:06:54 -0600

    Final Changes Part 3

--- backend/Mission11_Young.API/Data/Book.cs
+++ backend/Mission11_Young.API/Data/Book.cs
@@ -5,14 +5,23 @@ namespace Mission11_Young.API
     public class Book
     {
         [Key]
+        [Required]
         public int BookID { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Author { get; set; }
+        [Required]
         public string? Publisher { get; set; }
+        [Required]
         public string? ISBN { get; set; }
+        [Required]
         public string? Classification { get; set; }
+        [Required]
         public string? Category { get; set; }
+        [Required]
         public int? PageCount { get; set; }
+        [Required]
         public float? Price { get; set; }
     }
 }
