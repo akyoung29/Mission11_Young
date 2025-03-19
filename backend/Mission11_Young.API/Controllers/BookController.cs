1959f974b067993a92165f99f2e3488ea36e1f03
annay <akyoung@byu.edu>
2025-03-19 14:49:36 -0600

    Page

--- backend/Mission11_Young.API/Controllers/BookController.cs
+++ backend/Mission11_Young.API/Controllers/BookController.cs
@@ -16,9 +16,23 @@ namespace Mission11_Young.API.Controllers
             _context = temp;
         }
 
-        public IEnumerable<Book> GetBooks()
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1)
         {
-            return _context.Books.ToList();
+            var something = _context.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum-1)*pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+            
+            var totalNumBooks = _context.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                TotalNumBooks = totalNumBooks
+            };
+
+            return Ok(someObject);
         }
     }
 }
